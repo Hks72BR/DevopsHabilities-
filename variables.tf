77ea2ab70e5007e8f2cf05e481450500cb79f02d
Higor Coelho do Nascimento <35745919+Hks72BR@users.noreply.github.com>
2024-09-03 19:49:14 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
-variable "aws_access_key" {
-  description = "Access key for AWS"
-  type        = string
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
-variable "aws_secret_key" {
-  description = "Secret key for AWS"
-  type        = string
-  sensitive   = true
+variable "public_subnet_cidr_block_a" {
+  description = "CIDR block for the public subnet in AZ A"
+  default     = "10.0.1.0/24"
 }
 
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-west-2"
+variable "public_subnet_cidr_block_b" {
+  description = "CIDR block for the public subnet in AZ B"
+  default     = "10.0.2.0/24"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  default     = "devops-cluster"
 }
