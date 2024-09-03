65a3f5c5ee1928f8e84c334407b33d74c9e96c9f
Higor Coelho do Nascimento <35745919+Hks72BR@users.noreply.github.com>
2024-09-03 19:48:54 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,57 +1,102 @@
 provider "aws" {
-  access_key = "var.aws_access_key"
-  secret_key = "var.aws_secret_key"
+  access_key = "var.access_key" #AKIAYEKP5MF34TLP8DUG Example
+  secret_key = "var.secret_key" ##afwS2XkJ//agqKskpFTCeEzXWhLy+DsZ5DVGzkdF Example
   region     = "us-west-2"
 }
 
-
-resource "aws_vpc" "main" {
+resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
-}
 
-resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  map_public_ip_on_launch = true
-  availability_zone = element(["us-west-2a", "us-west-2b"], count.index)
-}
-
-resource "aws_subnet" "private" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 2)
-  availability_zone = element(["us-west-2a", "us-west-2b"], count.index)
-}
-
-resource "aws_eks_cluster" "k8s" {
-  name     = "cluster-Aplication"
-  role_arn = aws_iam_role.eks_cluster.arn
-  vpc_config {
-    subnet_ids = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
+  tags = {
+    Name = "example-vpc"
   }
 }
 
-resource "aws_iam_role" "eks_cluster" {
-  name = "eks-cluster-role"
+resource "aws_subnet" "example_a" {
+  cidr_block = "10.0.1.0/24"
+  vpc_id     = aws_vpc.example.id
+  availability_zone = "us-west-2a"
+
+  tags = {
+    Name = "example-subnet-a"
+  }
+}
+
+resource "aws_subnet" "example_b" {
+  cidr_block = "10.0.2.0/24"
+  vpc_id     = aws_vpc.example.id
+  availability_zone = "us-west-2b"
+
+  tags = {
+    Name = "example-subnet-b"
+  }
+}
+
+resource "aws_iam_role" "example" {
+  name        = "example-cluster-role"
+  description = "EKS cluster role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "eks.amazonaws.com"
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Effect = "Allow"
       }
-    }]
+    ]
   })
+
+  tags = {
+    Name = "example-iam-role"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_cluster.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+resource "aws_iam_policy" "example" {
+  name        = "example-cluster-policy"
+  description = "EKS cluster policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:*",
+          "elasticloadbalancing:*",
+          "cloudwatch:*",
+          "iam:PassRole",
+          "eks:*"
+        ]
+        Resource = "*"
+        Effect = "Allow"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "example-iam-policy"
+  }
 }
 
-output "cluster_endpoint" {
-  value = aws_eks_cluster.k8s.endpoint
+resource "aws_iam_role_policy_attachment" "example" {
+  role       = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.example.arn
+}
+
+resource "aws_eks_cluster" "example" {
+  name     = "example-cluster"
+  role_arn = aws_iam_role.example.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.example_a.id,
+      aws_subnet.example_b.id
+    ]
+  }
+
+  tags = {
+    Name = "example-eks-cluster"
+  }
 }
